4bc39ed76211247df50e6a02adcd423a0451e45f
alujuan <juanpeterssonb@gmail.com>
2023-04-18 16:32:15 -0300

    Modificação da categoria livro

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from livraria.models import Categoria, Editora, Autor
+from livraria.models import Categoria, Editora, Autor, Livro
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
+admin.site.register(Livro)
