99f6d925984dd4e30c8f63e1df9d13f71bb23e56
alujuan <juanpeterssonb@gmail.com>
2023-04-25 16:34:31 -0300

    Criação da api-editora

--- livraria/serializers.py
+++ livraria/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 
 from livraria.models import Categoria
+from livraria.models import Editora
 
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
+        fields = "__all__"
+
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
         fields = "__all__"
