10ee9637de22a1096311d5f2125445f795264e8d
alujuan <juanpeterssonb@gmail.com>
2023-04-27 13:41:30 -0300

    Api para livro

--- livraria/serializers.py
+++ livraria/serializers.py
@@ -2,6 +2,7 @@ from rest_framework.serializers import ModelSerializer
 
 from livraria.models import Categoria
 from livraria.models import Editora
+from livraria.models import Livro
 
 
 class CategoriaSerializer(ModelSerializer):
@@ -13,4 +14,10 @@ class CategoriaSerializer(ModelSerializer):
 class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
+        fields = "__all__"
+
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
         fields = "__all__"
--- livraria/views.py
+++ livraria/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 from livraria.models import Categoria
-from livraria.serializers import CategoriaSerializer
 from livraria.models import Editora
+from livraria.models import Livro
+
+from livraria.serializers import CategoriaSerializer
 from livraria.serializers import EditoraSerializer
+from livraria.serializers import LivroSerializer
 
 
 class CategoriaViewSet(ModelViewSet):
@@ -16,3 +19,8 @@ class CategoriaViewSet(ModelViewSet):
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
+
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
